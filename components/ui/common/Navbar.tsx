2ac37144ee147fb23060ac1bdd1c3476c2654e1c
DevAliSa <alidahniem@gmail.com>
2026-02-07 21:19:53 +0300

    Adding Buttons

--- components/ui/common/Navbar.tsx
+++ components/ui/common/Navbar.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import {  Sparkle } from 'lucide-react'
 import Link from 'next/link';
+import { buttonVariants } from '../button';
 
 export const Navbar = () => {
-  return (
+    const user = false
+    return (
     <header>
         <MaxWidthWrapper>
             <div className='flex justify-between items-center h-16'>
@@ -12,10 +14,35 @@ export const Navbar = () => {
                     <samp className="text-xl font-bold text-primary">
                     Muashir
                     </samp>
-                </Link>
+                </Link> 
+                <nav className="hidden md:flex items-center space-x-4">
+                {!user ? (
+                    <>
+                    <Link href="/login" className={buttonVariants({variant: "secondary"})}>
+                        Login
+                        </Link>
+                        <Link href="/register" className={buttonVariants()}>
+                            Sign up
+                        </Link>
+                        </>
+                    ):( 
+                    <div className="flex items-center gap-2">
+                        <Link href="/dashboard"
+                            className={buttonVariants({variant: "secondary"})}
+                            >
+                            Dashboard
+                            </Link>
+                            <Link className={buttonVariants()} href={"/create"}>
+                                Create
+                            </Link>
+                            
+                        </div>    
+                    )
+                }
+                </nav>
             </div>
         </MaxWidthWrapper>
     </header>
-  )
+    )
 }
 export default Navbar
