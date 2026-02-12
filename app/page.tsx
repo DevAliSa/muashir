fe53438488a0b6863a18d9a5427714e0395b9eb2
DevAliSa <alidahniem@gmail.com>
2026-02-13 01:31:22 +0300

    Adding Card Effect

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 function HeroSection() {
   return (
     <div className="py-20 md:py-36 text-center mx-8 ">
-      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-400 to-neutral-600 dark:from-neutral-600 dark:to-neutral-900 text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+      <h2 className="bg-clip-text text-transparent text-center bg-linear-to-b from-neutral-400 to-neutral-600 dark:from-neutral-600 dark:to-neutral-900 text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
         Your crypto in compliance <br /> with <Cover> Saudi standards </Cover>
       </h2>
       <p className="mt-6 animate-fade-in text-lg text-muted-foreground max-w-3xl mx-auto">
@@ -37,10 +37,40 @@ function HeroSection() {
 }
 
 
-
-
-
 function SampleCard() {
+  return (
+    <div className=" pb-36">
+      <h2 className="text-3xl animate-fade-in font-normal text-center mb-8 sm:text-2xl md:text-3xl">
+      Why choose us?
+      </h2>
+      <div className="grid animate-fade-in grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-8 ">
+
+        <div className="card-wrapper h-38 ">
+          <CardTitle className="card-content p-4 text-lg font-bold mb-2">Comprehensive Management
+          <p className=" font-normal text-sm">Track all your crypto assets in one place with real-time updates and insights.</p></CardTitle>
+        </div>
+
+        <div className="card-wrapper h-38 ">
+          <CardTitle className="card-content p-4 text-lg font-bold mb-2">Zakat Calculation Made Easy 
+          <p className=" font-normal text-sm">Automatically calculate your Zakat obligations based on your crypto holdings and transactions.</p></CardTitle>
+        </div>
+
+
+        <div className="card-wrapper h-38 ">
+          <CardTitle className="card-content p-4 text-lg font-bold mb-2">Regulatory Compliance
+          <p className=" font-normal text-sm">Stay compliant with Saudi financial regulations through our integrated compliance tools and resources.</p></CardTitle>
+        </div>
+        
+      </div>
+    </div>
+
+
+
+  );
+}
+
+
+/*function SampleCard() {
   return (
     <div className=" pb-36">
       <h2 className="text-3xl animate-fade-in font-normal text-center mb-8 sm:text-2xl md:text-3xl">
@@ -72,7 +102,7 @@ function SampleCard() {
 
   );
 }
-
+*/
 
 
 
