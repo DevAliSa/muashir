ff0480835028c17a807fa9956475ce817427c0ef
DevAliSa <alidahniem@gmail.com>
2026-02-26 23:08:48 +0300

    RTL 05

--- components/ui/common/Navbar.tsx
+++ components/ui/common/Navbar.tsx
@@ -5,11 +5,21 @@ import Link from 'next/link';
 import { Button, buttonVariants } from '../button';
 import { useTheme } from 'next-themes';
 import MobileMenu from './MobileMenu';
+import { usePathname, useRouter } from 'next/navigation';
 
 
 export const Navbar = () => {
     const {theme, setTheme} = useTheme();
-    const user = false
+    const pathname = usePathname();
+    const router = useRouter();
+    const user = false;
+    
+    const toggleLocale = () => {
+        const currentLang = pathname.split("/")[1];
+        const newLang = currentLang === "en" ? "ar" : "en";
+        const newPath = pathname.replace(`/${currentLang}`, `/${newLang}`);
+        router.push(newPath);
+    };
     return (
     <header className="fixed top-0 z-50 w-full animate-fade-in backdrop-blur-md ">
         <MaxWidthWrapper>
@@ -20,27 +30,28 @@ export const Navbar = () => {
                     Muashir
                     </div>
                 </Link> 
-                <MobileMenu user={user} />
-                <nav className="hidden md:flex items-center space-x-4">
-                {!user ? (
-                    <>
-                        <Link href="/" className={buttonVariants({variant: "secondary", size: "icon"})} >
-                            Ar
-                        </Link>
+                <div className="md:hidden">
+                    <MobileMenu user={user} />
+                </div>
+                <nav className="hidden md:flex items-center gap-4">
+                        <Button  variant="secondary" size="icon"onClick={toggleLocale}>
+                            {pathname.startsWith('/ar') ? 'En' : 'Ar'}
+                        </Button>
                         <Button variant="secondary" size="icon" className="rounded-full"
                             onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
                             <Moon size={18} className="rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
                             <Sun size={18} className="absolute rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
                         </Button>
-                        <Link href="/login" className={buttonVariants({variant: "secondary"})}>
-                        Login
-                        </Link>
-                        <Link href="/register" className={buttonVariants()}>
-                            Sign up
-                        </Link>
-                        
-                        </>
-                    ):( 
+                        {!user ? (
+                            <div className="flex items-center gap-2">
+                                <Link href="/login" className={buttonVariants({variant: "secondary"})}>
+                                    Login
+                                </Link>
+                                <Link href="/register" className={buttonVariants()}>
+                                    Sign up
+                                </Link>
+                            </div>
+                    ):(
                     <div className="flex items-center gap-2">
                         <Link href="/dashboard"
                             className={buttonVariants({variant: "secondary"})}
