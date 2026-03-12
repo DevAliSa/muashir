cbcc1587e2c152ae53363677cd8cc913da5803e9
DevAliSa <alidahniem@gmail.com>
2026-03-12 23:11:08 +0300

    dashboard

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -10,11 +10,6 @@ import { ThemeProvider } from "next-themes";
 import { routing } from "@/src/i18n/routing";
 import { notFound } from "next/navigation";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
 const Watad = LocalFont({
   src: [{
     path: '../../../public/fonts/Watad.otf'
