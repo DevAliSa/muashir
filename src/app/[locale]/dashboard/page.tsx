5119d10cc1fda760dfc887b8da92ed36d6431a23
DevAliSa <alidahniem@gmail.com>
2026-04-27 17:19:31 +0300

    data table

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -2,6 +2,8 @@ import MaxWidthWrapper from "@/components/ui/common/MaxWidthWrapper";
 import React from "react";
 import Image from "next/image";
 import { setRequestLocale } from "next-intl/server";
+import { CardTitle } from "@/components/ui/card";
+import DataTable from "@/components/ui/DataTaable";
 
 export default async function DashboardPage({
   params,
@@ -14,13 +16,15 @@ export default async function DashboardPage({
     <main className="relative min-h-screen overflow-hidden">
       <MaxWidthWrapper className="relative z-10">
             <div className="py-20 pt-2 md:py-36 text-center animate-fade-in">
-                <div className="flex  items-center gap-4 p-8 rounded-2xl bg-black/5 dark:bg-white/5 border border-black/10 dark:border-white/10 backdrop-blur-sm shadow-sm">
+                <div className="  flex  items-center gap-2 p-8 rounded-2xl bg-black/5 dark:bg-white/5 border border-black/10 dark:border-white/10 backdrop-blur-sm ">
                   <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" width={36} height={36}/>
-                  <div className="info">
-                    <p>BitCoin</p>
+                  <CardTitle className="text-left">
+                    <p className="font-light">BitCoin / BTC</p>
                     <h1>$89,113.00</h1>
-                  </div>
+                  </CardTitle>
                 </div>
+                <p>Trending Coins</p>
+                 <DataTable />
             </div>
               </MaxWidthWrapper>
     </main>
