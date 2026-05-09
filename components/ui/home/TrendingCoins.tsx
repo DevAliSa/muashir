4a24c8f723ee8d359d9b6f0fb94347938f8aac15
DevAliSa <alidahniem@gmail.com>
2026-05-09 19:52:42 +0300

    Link trending items to their respective coin detail page

--- components/ui/home/TrendingCoins.tsx
+++ components/ui/home/TrendingCoins.tsx
@@ -1,10 +1,11 @@
 import { fetcher } from '@/src/lib/coingecko.actions';
 import MaxWidthWrapper from "@/components/ui/common/MaxWidthWrapper";
 import Image from "next/image";
+import Link from "next/link";
 import { setRequestLocale } from "next-intl/server";
 import { CardTitle } from "@/components/ui/card";
 import DataTable from "@/components/ui/DataTable";
-import { TrendingDown, TrendingUp } from "lucide-react";
+import {  TrendingDown, TrendingUp } from "lucide-react";
 import { cn, formatCurrency } from "@/components/ui/utils";
 
 const TrendingCoins = async () => {
@@ -16,8 +17,43 @@ const TrendingCoins = async () => {
       cellClassNme: 'name-cell',
       cell: (coin) => {
         const item = coin.item;
+
+        return (
+          <Link href={`/coins/${item.id}`}>
+            <Image src={item.large} alt={item.name} width={36} height={36}/>
+            <p>{item.name}</p>
+          </Link>
+        );
       },
-    ];
+    },
+      {
+        header: '24h Change',
+        cellClassName: 'change-cell',
+        cell: (coin) => {
+          const item = coin.item;
+          const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
+
+          return (
+            <div className={cn('price-change', isTrendingUp? 'text-green-500' : 'text-red-500')}>
+              <p>
+                {isTrendingUp ?( <TrendingUp width={16} height={16}/>
+                ) : (
+                  <TrendingDown width={16} height={16}/>
+                )}
+                {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
+              </p>
+            </div>
+          );
+              },
+
+            },
+
+            {
+              header: 'Price',
+              cellClassName: 'price-cell',
+              cell: (coin) => formatCurrency( coin.item.data.price),
+            },
+          ];
 
   return (
     <div id= "trending-coins">
